bc1818ec5af4d91cd167677fe719e8551ae4d57d
rizwanlodhi <rizwan.lodhi@gmail.com>
2026-01-19 11:56:50 -0600

    First cut.

---
+++ main.py
@@ -0,0 +1,15 @@
+import csv
+
+with open("weather_data.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    # weather_data = list(csv_reader)
+    # print(weather_data)
+    temperatures = []
+    for row in csv_reader:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+
+    print(temperatures)
+
+
+
