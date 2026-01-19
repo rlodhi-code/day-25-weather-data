7deedfef4d5fa94b85cc0b30a90ee137526db5ee
rizwanlodhi <rizwan.lodhi@gmail.com>
2026-01-19 14:52:53 -0600

    Changed main.py to use dict from pandas.

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import csv
+# import csv
+#
+# with open("weather_data.csv") as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     # weather_data = list(csv_reader)
+#     # print(weather_data)
+#     temperatures = []
+#     for row in csv_reader:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#
+#     print(temperatures)
 
-with open("weather_data.csv") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    # weather_data = list(csv_reader)
-    # print(weather_data)
-    temperatures = []
-    for row in csv_reader:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
+import pandas
 
-    print(temperatures)
+data = pandas.read_csv("weather_data.csv")
 
+data_dict = data.to_dict()
+
+print(data_dict)
 
 
